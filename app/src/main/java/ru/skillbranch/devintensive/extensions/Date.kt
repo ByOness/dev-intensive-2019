d66b7cfa19da4929a0a4afbf4e725dcb35ac7765
ByOne <parcor.ru@gmail.com>
2019-07-10 20:01:58 +0700

    hometask_2 complete3

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,29 +1,67 @@
 package ru.skillbranch.devintensive.extensions
 
-import java.lang.IllegalStateException
+import java.lang.Math.abs
 import java.text.SimpleDateFormat
 import java.util.*
-
-const val SECOND = 1000L
-const val MINUTE = 60 * SECOND
-const val HOUR = 60 * MINUTE
-const val DAY = 24 * HOUR
 
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
 }
 
-fun Date.add(value: Int, TimeUnits: String): Date {
-    var time = this.time
-
-    when (TimeUnits) {
-        "second", "seconds" -> value * SECOND
-        "minute", "minutes" -> value * MINUTE
-        "hour", "hours" -> value * HOUR
-        "day", "days" -> value * DAY
-        else -> throw IllegalStateException("invalid units")
-    }
-    this.time = time
+fun Date.add(value: Long, units: TimeUnits): Date {
+    this.time += units * value
     return this
+}
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY;
+
+    operator fun times(value: Long):Long {
+        return value * when(this) {
+            SECOND -> 1000L
+            MINUTE -> SECOND * 60
+            HOUR -> MINUTE * 60
+            DAY -> HOUR * 24
+        }
+    }
+
+    fun plural(value:Long):String {
+        return "${abs(value)} " + when (this) {
+            SECOND -> {when (pluralForm(value)) {
+                0 -> "секунду"
+                1 -> "секунды"
+                else -> "секунд"
+            }
+            }
+            MINUTE -> {when (pluralForm(value)) {
+                0 -> "минуту"
+                1 -> "минуты"
+                else -> "минут"
+            }
+            }
+            HOUR -> {when (pluralForm(value)) {
+                0 -> "час"
+                1 -> "часа"
+                else -> "часов"
+            }
+            }
+            DAY -> {when (pluralForm(value)) {
+                0 -> "день"
+                1 -> "дня"
+                else -> "дней"
+            }
+            }
+        }
+    }
+
+    private fun pluralForm(value:Long):Int {
+        val absvalue = abs(value)
+        return if (absvalue%10==1L && absvalue%100!=11L) 0
+        else if (absvalue%10>=2L && absvalue%10<=4L && (absvalue%100<10L || absvalue%100>=20L)) 1
+        else 2
+    }
 }
